85431c47191be18ac76767cdc670593534cf304c
nguyenxuandaipro <79356807+nguyenxuandaipro@users.noreply.github.com>
2022-06-11 21:17:08 +0700

    Update xuandai.sh
    ok

--- xuandai.sh
+++ xuandai.sh
@@ -1,8 +1,11 @@
-echo "Download ngrok..."
-wget https://raw.githubusercontent.com/nguyenxuandaipro/xuandaipro/main/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
-unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
-echo "Copy&paste your tunnel authtoken from https://dashboard.ngrok.com/auth"
-read -p "Paste Ngrock Authtoken and Enter: " CRP
+echo "======================="
+echo "Download ngrok"
+echo "======================="
+rm -rf ngrok ngrok.zip ngrok.sh > /dev/null 2>&1
+wget -O ng.sh https://bit.ly/GCngrok > /dev/null 2>&1
+chmod +x ng.sh
+./ng.sh
+clear
 echo "======================="
 echo choose ngrok region
 echo "======================="
@@ -16,32 +19,3 @@ echo "in - India (Mumbai)"
 read -p "choose ngrok region: " CRP
 ./ngrok tcp --region $CRP 3389 &>/dev/null &
 echo "======================="
-echo Please wait downloading file install...
-echo "Chờ khoảng 5 phút, nếu thấy lâu quá chưa hiện IP bấm Enter cái"
-sudo apt-get update > /dev/null 2>&1
-echo "Install App..."
-sudo apt install firefox -y > /dev/null 2>&1
-sudo apt-get install axel -y > /dev/null 2>&1
-cd /tmp
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb > /dev/null 2>&1
-sudo dpkg -i google-chrome-stable_current_* > /dev/null 2>&1
-sudo apt-get -f install > /dev/null 2>&1
-sudo apt-get install xarchiver > /dev/null 2>&1
-sudo apt install mkvtoolnix mkvtoolnix-gui > /dev/null 2>&1
-dpkg --add-architecture i386 && apt-get update && apt-get install wine32 > /dev/null 2>&1
-apt-get install wine32 > /dev/null 2>&1
-sudo snap install notepad-plus-plus -y > /dev/null 2>&1
-sudo apt-get install terminator > /dev/null 2>&1
-curl https://rclone.org/install.sh | sudo bash > /dev/null 2>&1
-echo "Install RDP..."
-sudo apt install -y xrdp > /dev/null 2>&1
-sudo apt-get install xfce4 xfce4-terminal -y > /dev/null 2>&1
-echo "Start RDP..."
-sudo sed -i.bak '/fi/a xfce4-session \n' /etc/xrdp/startwm.sh > /dev/null 2>&1
-sudo service xrdp start > /dev/null 2>&1
-echo RDP Address:
-curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
-echo "===================================="
-echo "Không Đóng TAB này"
-echo "===================================="
-sleep 43210
